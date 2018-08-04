708a0951bba946ba945354d85aceaa2c6b2f3554
JoniAguero <aguerojonathanivan@gmail.com>
2018-08-04 11:16:43 -0300

    Estadisticas added

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../ngrx/app.reducer';
+import { IngresoEgreso } from '../../models/IngresoEgreso.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  private montoIngreso = 0;
+  private montoEgreso = 0;
+
+  private cantIngreso = 0;
+  private cantEgreso = 0;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('ie').subscribe( data => {
+      this.calcularValores(data.ie);
+    });
+  }
+
+  calcularValores( data: IngresoEgreso [] ) {
+    data.forEach( element => {
+      if (element.tipo === 'ingreso') {
+        this.cantIngreso ++;
+        this.montoIngreso += element.monto;
+      } else {
+        this.cantEgreso++;
+        this.montoEgreso += element.monto;
+      }
+    });
   }
 
 }
