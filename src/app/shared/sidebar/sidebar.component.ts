34ad7f885d6d303f768c93fddbc1e30d6b98e3f2
JoniAguero <aguerojonathanivan@gmail.com>
2018-08-04 11:45:16 -0300

    Nombre User
    Se agrega nombre de usuario en navbar y sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from '../../auth/auth.service';
 import { AppState } from '../../ngrx/app.reducer';
 import { NotLoadingUIAction } from '../../ngrx/UI/ui.actions';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'node_modules/rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  nombre: string;
+  subscription: Subscription = new Subscription();
 
   constructor(private _authService: AuthService,
               private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('user')
+      .pipe(filter(res => res.user !== null))
+      .subscribe(res => {
+        this.nombre = res.user.nombre;
+      });
   }
 
   signOut() {
     this._authService.logout();
     const action = new NotLoadingUIAction();
     this.store.dispatch(action);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
